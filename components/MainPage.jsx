9163d8a8f832dd89152623dc0dae24ad6fed22a5
bakifra <bakifra@mail.ru>
2023-06-02 12:16:24 +0300

    add MainPage.jsx

---
+++ components/MainPage.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+const Layout = require('./Layout');
+
+function MainPage({title, header}) {
+  return(
+  <Layout title= {title}>
+
+  </Layout>
+  )
+};
+module.exports = MainPage;
